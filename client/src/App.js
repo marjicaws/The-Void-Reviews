87f7ed2f5fdaff677a434aa35cd2a7aec787e76c
Marjica Watson-Santos <marjicawatson-santos@Marjicas-MBP.attlocal.net>
2021-12-03 11:41:34 -0500

    Working

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-//import { Routes , Route } from 'react-router-dom'
+import { Routes , Route , Link } from 'react-router-dom'
 import { useEffect , useState } from 'react'
 import { grabHorror } from './services/index'
+import Detail from './components/Detail'
 
 
 function App() {
@@ -22,9 +23,15 @@ function App() {
   return (
     <div className="App">
       
-      {/* <Routes>
-
-      </Routes> */}
+      <Routes>
+        <Route path='/' element={
+          <div>
+            {horrorData.map(hd => (
+              <Link to={`/detail/${hd.id}`}>{hd.fields.imgURL}</Link>
+            ))}
+          </div>
+        }/>
+      </Routes> 
     </div>
   );
 }
