786d33060f8b73a9b5da27812f55ee334b2e27fd
Marjica Watson-Santos <marjicawatson-santos@Marjicas-MBP.attlocal.net>
2021-12-03 10:27:08 -0500

    Working

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,25 @@
 import './App.css';
-import { Routes , Route } from 'react-router-dom'
+//import { Routes , Route } from 'react-router-dom'
 import { useEffect , useState } from 'react'
 import axios from 'axios'
+import {BASE_URL, config} from './services/index'
 
 
 function App() {
+
+  const [horrorData, setHorrorData] = useState({})
+
+  useEffect(() => {
+    
+  }, [])
+
+
   return (
     <div className="App">
       
-      <Routes>
+      {/* <Routes>
 
-      </Routes>
+      </Routes> */}
     </div>
   );
 }
--- client/src/services/index.js
+++ client/src/services/index.js
@@ -1,1 +1,11 @@
 // CRUD REQUESTS HERE
+
+const BASE = process.env.HORROR_REVIEWS_AIRTABLE_BASE
+
+const BASE_URL = `https://api.airtable.com/v0/${BASE}/horror`
+const config = {
+    headers: {
+        Authorization: `Bearer ${process.env.HORROR_REVIEWS_AIRTABLE_KEY}`
+    }
+}
+
