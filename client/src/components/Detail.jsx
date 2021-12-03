1e67047b3598c171b8486e4963021d7cb4761a28
Marjica Watson-Santos <marjicawatson-santos@Marjicas-MBP.attlocal.net>
2021-12-03 14:39:38 -0500

    Working

--- client/src/components/Detail.jsx
+++ client/src/components/Detail.jsx
@@ -1,17 +1,16 @@
-import { useParams } from 'react-router-dom'
+import { useParams } from "react-router-dom";
 
-export default function Detail () {
+export default function Detail(props) {
+  const params = useParams();
+  const horrors = props.horrorData.find((hd) => {
+    return params.id === hd.id;
+  });
 
-
-// const params = useParams()
-//   const spookyDetail = props.horrors.find((horror) => {
-//     return params.title === horror.title
-//   })
-//   console.log(spookyDetail)
-  
-    return (
-       <div>
-  <h3>Spooky!</h3>
-       </div> 
-    )
+  return (
+    <div>
+      <h2>{horrors.fields.movie}</h2>
+      <p>{horrors.fields.synopsis}</p>
+      <img src={horrors.fields.imgURL}></img>
+    </div>
+  );
 }
