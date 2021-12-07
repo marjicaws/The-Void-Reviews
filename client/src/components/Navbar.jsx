9706381d9572d85a7ddf2371e154b8de5833697b
Marjica Watson-Santos <marjicawatson-santos@Marjicas-MBP.attlocal.net>
2021-12-07 17:54:06 -0500

    Working

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,17 +2,19 @@ import { Link } from "react-router-dom";
 
 export default function Navbar(props) {
   return (
+    <nav>
     <div className="nav">
       <Link class="text-dark" to="/">
         The Void
       </Link>
-      <ul class="nav justify-content-end" className="submitlink">
+      <ul class="nav justify-content-end" >
         <li class="nav-item">
-          <Link class="text-dark" to="/new">
+          <Link class="text-dark" className="submitlink" to="/new">
             Submit New Movie
           </Link>
         </li>
       </ul>
     </div>
+    </nav>
   );
 }
